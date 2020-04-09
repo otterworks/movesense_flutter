f1da423035bb744e0b4db9a855081547ef872f8f
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2020-04-08 19:21:47 -0700

    get String from invokeMethod; make WhiteboardResponse from String

--- lib/movesense_flutter.dart
+++ lib/movesense_flutter.dart
@@ -41,45 +41,63 @@ class MovesenseFlutter {
   // TODO: consider separate MethodChannel for get,put,post,delete
 
   static Future<String> get info async {
-    final WhiteboardResponse response = await _mc.invokeMethod('get', {'path':'/Info'});
-    return response.content;
+    final String response = await _mc.invokeMethod('get', {'path':'/Info'});
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
+    if( wr.response == 200 ) {
+      return json.encode(wr.content); // re-encode for now...
+    } else {
+      return wr.responseString;
+    }
   }
 
   static Future<String> get appInfo async {
-    final WhiteboardResponse response = await _mc.invokeMethod('get', {'path':'/Info/App'});
-    return response.content;
+    final String response = await _mc.invokeMethod('get', {'path':'/Info/App'});
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
+    if( wr.response == 200 ) {
+      return json.encode(wr.content); // re-encode for now...
+    } else {
+      return wr.responseString;
+    }
   }
 
   static Future<String> get batteryLevel async {
-    final WhiteboardResponse response = await _mc.invokeMethod('get', {'path':'/System/Energy/Level'});
-    return response.content;
+    final String response = await _mc.invokeMethod('get', {'path':'/System/Energy/Level'});
+    return response;
   }
 
   static Future<int> get getTime async {
-    final WhiteboardResponse response = await _mc.invokeMethod('get', {'path':'/Time'});
+    final String response = await _mc.invokeMethod('get', {'path':'/Time'});
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
     int utime = 0;
-    print(response.content);
-    if(response.status == 200 ) {
-      utime = int.parse(response.content);
+    print("content: ${wr.content}, type: ${wr.content.runtimeType}");
+    if(wr.response == 200 ) {
+      utime = 1e6.toInt(); // .parse(response.content);
     }
     return utime;
   }
 
   static Future<String> get getDetailedTime async {
-    final WhiteboardResponse response = await _mc.invokeMethod('get', {'path':'/Time/Detailed'});
-    return response.content;
+    final String response = await _mc.invokeMethod('get', {'path':'/Time/Detailed'});
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
+    if(wr.response == 200 ) {
+      return json.encode(wr.content);
+    } else {
+      return wr.responseString;
+    }
   }
 
   static Future<Null> setTime(int utime) async {
-    final WhiteboardResponse response = await _mc.invokeMethod('put', {'path':'/Time', 'type':'int64', 'value':utime}); // the key `value` does not appear to be officially documented
-    if ( response.status != 200 ) {
+    final String response = await _mc.invokeMethod('put', {'path':'/Time', 'type':'int64', 'value':utime}); // the key `value` does not appear to be officially documented
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
+    if ( wr.response != 200 ) {
       print("setting time unsuccessful: `$response`");
     }
   }
 
   static Future<Null> setVisualIndicator(int mode) async {
-    final WhiteboardResponse response = await _mc.invokeMethod('put', {'path':'/Ui/Ind/Visual', 'type':'uint16', 'newState':mode}); // the key `newState` does not appear to be officially documented
-    if ( response.status != 200 ) {
+    final String response = await _mc.invokeMethod('put', {'path':'/Ui/Ind/Visual', 'type':'uint16', 'newState':mode}); // the key `newState` does not appear to be officially documented
+    final WhiteboardResponse wr = WhiteboardResponse.fromJsonString(response);
+    if ( wr.response != 200 ) {
       print("setting time unsuccessful: `$response`");
     }
   }
